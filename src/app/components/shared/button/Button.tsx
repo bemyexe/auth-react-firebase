a4416b9ece4bdd27ba2ef74690f66e8a89cc65ba
maxim <trifonov.max@mail.ru>
2024-04-07 22:37:45 +0300

    feature(button): added button component

---
+++ src/app/components/shared/button/Button.tsx
@@ -0,0 +1,49 @@
+import classnames from "classnames";
+import Loader from "../loader/Loader";
+import "./Button.module.scss";
+
+type ButtonSize = "s" | "m" | "l";
+
+type StyleType = "filled" | "outlined" | "translucent" | "transparent";
+
+type ButtonColor = "red" | "green" | "blue" | "orange";
+
+interface ButtonProps {
+  size?: ButtonSize;
+  styleType?: StyleType;
+  color?: ButtonColor;
+  onClick: () => void;
+  disabled?: boolean;
+  children: React.ReactNode;
+  loading?: boolean;
+  className?: string;
+  title: string;
+  type?: "button" | "submit" | "reset";
+}
+
+const Button: React.FC<ButtonProps> = ({
+  onClick,
+  styleType = "filled",
+  color = "blue",
+  size = "s",
+  disabled,
+  children,
+  loading,
+  className,
+  title,
+  type = "button",
+}) => {
+  return (
+    <button
+      className={classnames("button", styleType, color, size, className)}
+      onClick={onClick}
+      disabled={disabled || loading}
+      title={title}
+      type={type}
+    >
+      {loading ? <Loader /> : children}
+    </button>
+  );
+};
+
+export default Button;
