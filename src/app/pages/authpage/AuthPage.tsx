122e43ed9912b01c9df1788333aab021d0f6f50e
maxim <trifonov.max@mail.ru>
2024-04-08 17:49:30 +0300

    feature(pages): added auth/reg pages

--- src/app/pages/authpage/AuthPage.tsx
+++ src/app/pages/authpage/AuthPage.tsx
@@ -1,11 +1,68 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Input from "../../components/shared/input/Input";
+import { useState } from "react";
+import Button from "../../components/shared/button/Button";
+import { useDispatch } from "react-redux";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { setUser } from "../../store/slices/user-slices";
+import classes from "./AuthPage.module.scss";
+import {
+  TextLLight,
+  TextLSemibold,
+} from "../../components/shared/typography/Typography";
 
 const Authpage = () => {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const handleLogin = (email: any, password: any) => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then(({ user }) => {
+        dispatch(
+          setUser({
+            email: user.email,
+            id: user.uid,
+            token: user.accessToken,
+          })
+        );
+        navigate("/home");
+      })
+      .catch(console.error);
+  };
+
   return (
-    <>
-      <h1>auth</h1> dont have an acc?
-      <Link to="/signup">Sign up!</Link>
-    </>
+    <div className={classes.authContent}>
+      <TextLSemibold>Auth</TextLSemibold>
+      <Input
+        title="Email Address"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="email"
+      />
+      <Input
+        title="Password"
+        value={pass}
+        onChange={(e) => setPass(e.target.value)}
+        placeholder="password"
+        type="password"
+      />
+      <Button
+        onClick={() => handleLogin(email, pass)}
+        children={"Sign in"}
+        title={"Sign in"}
+      />
+      <TextLLight>Don't have an account yet?</TextLLight>
+      <Button
+        styleType="transparent"
+        onClick={() => {}}
+        children={<Link to="/signup">Create Account</Link>}
+        title="create account"
+      />
+    </div>
   );
 };
 
--- src/app/pages/signuppage/SignUpPage.tsx
+++ src/app/pages/signuppage/SignUpPage.tsx
@@ -1,11 +1,65 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Input from "../../components/shared/input/Input";
+import { useState } from "react";
+import Button from "../../components/shared/button/Button";
+import { useDispatch } from "react-redux";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { setUser } from "../../store/slices/user-slices";
+import {
+  TextLLight,
+  TextLSemibold,
+} from "../../components/shared/typography/Typography";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const handleRegister = (email: any, password: any) => {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(({ user }) => {
+        dispatch(
+          setUser({
+            email: user.email,
+            id: user.uid,
+            token: user.accessToken,
+          })
+        );
+        navigate("/home");
+      })
+      .catch(console.error);
+  };
+
   return (
     <>
-      <h1>sign up</h1>
-      already have acc?
-      <Link to="/auth">Sign in!</Link>{" "}
+      <TextLSemibold>Sign up!</TextLSemibold>
+      <Input
+        title="Email Address"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="email"
+      />
+      <Input
+        title="Password"
+        value={pass}
+        onChange={(e) => setPass(e.target.value)}
+        placeholder="password"
+      />
+      <Button
+        onClick={() => handleRegister(email, pass)}
+        children={"Sign up"}
+        title={"title2"}
+      />
+      <TextLLight>Already have an account?</TextLLight>
+      <Button
+        styleType="transparent"
+        title={"Sign in!"}
+        onClick={() => {}}
+        children={<Link to="/auth">Sign in!</Link>}
+      />
     </>
   );
 };
