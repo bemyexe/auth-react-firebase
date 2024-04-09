f9850a43ecedbd833c0c5c6e69491777ad572904
maxim <trifonov.max@mail.ru>
2024-04-09 12:05:40 +0300

    feature(homepage): added adaptive layout

--- src/app/pages/homepage/HomePage.tsx
+++ src/app/pages/homepage/HomePage.tsx
@@ -2,20 +2,23 @@ import Button from "../../components/shared/button/Button";
 import { useAuth } from "../../hooks/useAuth";
 import { removeUser } from "../../store/slices/user-slices";
 import { useAppDispatch } from "../../hooks/redux-hooks";
+import "./HomePage.scss";
 
 const Homepage = () => {
   const dispatch = useAppDispatch();
   const { email } = useAuth();
 
   return (
-    <>
-      Hello, {email}!
-      <Button
-        onClick={() => dispatch(removeUser())}
-        children={"Log out!"}
-        title={"Log out!"}
-      />
-    </>
+    <div className="home-component">
+      <div className="home-content">
+        Hello, {email}!
+        <Button
+          onClick={() => dispatch(removeUser())}
+          children={"Log out!"}
+          title={"Log out!"}
+        />
+      </div>
+    </div>
   );
 };
 
